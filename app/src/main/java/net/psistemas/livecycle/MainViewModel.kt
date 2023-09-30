c81e5c0e277e9cfa3970e4163fa610b872cf9804
Anchieta Acacio <protonsnet@gmail.com>
2023-09-30 08:41:14 -0300

    Refatorado com eventos LiveData

--- app/src/main/java/net/psistemas/livecycle/MainViewModel.kt
+++ app/src/main/java/net/psistemas/livecycle/MainViewModel.kt
@@ -1,13 +1,25 @@
 package net.psistemas.livecycle
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    var counter = 0
-
-        private set
+    private val _counter = NumberLiveData()
+    val counter: LiveData<Int> = _counter
 
     fun increment(){
-        counter++
+        val number = _counter.value ?: 0
+        _counter.value = number + 1
+    }
+}
+
+class NumberLiveData(initial: Int=0) : MutableLiveData<Int>(initial){
+    override fun onActive() {
+        super.onActive()
+    }
+
+    override fun onInactive() {
+        super.onInactive()
     }
 }
